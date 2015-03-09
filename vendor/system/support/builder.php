d01e00ca420401eee976b3a8fd883f4fea88beb7
arul khairullah <arul.khairullah@gmail.com>
2015-03-09 15:42:15 +0700

    adding builder class

---
+++ vendor/system/support/builder.php
@@ -0,0 +1,13 @@
+<?php
+	
+class Builder{
+	
+	//PHP 5.2.0
+	public static function respond_to_json($tag, $content){
+		header('Content-type: application/json');
+		echo json_encode(array("'{$tag}'" => $content));
+	}
+	
+}
+	
+?>
