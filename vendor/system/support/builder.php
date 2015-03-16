03e6ec32e82a71a104bbd5c36dcdc91bfe8e9618
arul khairullah <arul.khairullah@gmail.com>
2015-03-16 18:58:09 +0700

    updating some codes

--- vendor/system/support/builder.php
+++ vendor/system/support/builder.php
@@ -5,7 +5,7 @@ class Builder{
 	//PHP 5.2.0
 	public static function respond_to_json($tag, $content){
 		header('Content-type: application/json');
-		echo json_encode(array("'{$tag}'" => $content));
+		echo json_encode(array("{$tag}" => $content));
 	}
 	
 }
--- vendor/system/support/utility.php
+++ vendor/system/support/utility.php
@@ -1,12 +1,39 @@
 <?php
-	
-function debugger($var){
-	echo "<b>Debugger:</b>";
-	echo "<hr>";
-	echo "<pre>";
-	var_dump($var);
-	echo "</pre>";
-	echo "<hr>";
+
+/**
+* function name debugger
+* this function using for debugging tool
+*/
+if (!function_exists('debugger')){
+	function debugger($var){
+		echo "<b>Debugger:</b>";
+		echo "<hr>";
+		echo "<pre>";
+		var_dump($var);
+		echo "</pre>";
+		echo "<hr>";
+	}
+}
+
+/**
+* function name getallheaders
+* this function using for getting header information
+*/
+if (!function_exists('getallheaders'))  {
+	function getallheaders()
+	{
+	    if (!is_array($_SERVER)) {
+	        return array();
+	    }
+
+	    $headers = array();
+	    foreach ($_SERVER as $name => $value) {
+	        if (substr($name, 0, 5) == 'HTTP_') {
+	            $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+	        }
+	    }
+	    return $headers;
+	}
 }
 	
 ?>
